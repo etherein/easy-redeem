79699579db5666c9d8c6c622121f65b3aeeab512
Qian <xinqian@live.com>
2022-05-09 17:10:43 +0800

    chore: change publicPath to work with gh-pages deployment

--- quasar.conf.js
+++ quasar.conf.js
@@ -36,7 +36,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/easy-redeem',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
